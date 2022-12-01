a29b890573cf209e0970fc7d84c72e0dad556865
Adrian Kunowski <adrian.kunowski@gmail.com>
2022-12-01 19:58:33 +0100

    day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun groupElvesCalories(input: String) =
+        input.split("\r\n\r\n")
+            .map { caloriesGroup ->
+                caloriesGroup.lines().map { it.toInt() }
+            }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: String): Int =
+        groupElvesCalories(input)
+            .maxOf { it.sum() }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: String): Int =
+        groupElvesCalories(input)
+            .map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
 
-    val input = readInput("Day01")
+
+    val input = File("src/inputs/day1.txt").readText()
     println(part1(input))
     println(part2(input))
 }
