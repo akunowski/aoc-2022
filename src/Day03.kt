33478ba1727dcf91a93de901886380ee0a2a2590
Adrian Kunowski <adrian.kunowski@gmail.com>
2022-12-03 20:07:04 +0100

    day3 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -11,23 +11,27 @@ fun main() {
         return leftCompartment to rightCompartment
     }
 
-    fun priority(it: Char): Int = if (it.code > 90) it.code - 96 else it.code - 38
+    fun priority(it: Char): Int =
+        if (it.isLowerCase())
+            it - 'a' + 1
+        else
+            it - 'A' + 27
 
     fun priority(it: Set<Char>): Int = it.sumOf { priority(it) }
 
     fun intersectAll(it: List<Set<Char>>): Set<Char> =
-        it.reduce { first, second -> first.intersect(second) }
+        it.reduce { first, second -> first intersect second }
 
     fun part1(input: List<String>): Int =
         input
             .map { compartmentsItems(it) }
-            .map { it.first.intersect(it.second) }
+            .map { it.first intersect it.second }
             .sumOf { priority(it) }
 
     fun part2(input: List<String>): Int =
         input
             .map { compartmentsItems(it) }
-            .map { it.first.plus(it.second) }
+            .map { it.first + it.second }
             .windowed(3, 3) { intersectAll(it) }
             .sumOf { priority(it) }
 
